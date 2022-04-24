1d4d0a1c4ec81c506cf576bd9239027ce31ef730
WTFender <wtfender.cs@gmail.com>
2022-04-24 14:49:24 -0500

    typo

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 go test -v || { echo -e 'STOP\tGo tests failed, stopped build' ; exit 1; }
 echo '=== BUILD'
-GOOS=linux   GOARCH=ppc64 go build -o ./bin/cmart.elf ./cmd/cmart/ && echo -e 'ok\t./bin/cmart.elf'
+GOOS=linux   GOARCH=amd64 go build -o ./bin/cmart.elf ./cmd/cmart/ && echo -e 'ok\t./bin/cmart.elf'
 GOOS=windows GOARCH=amd64 go build -o ./bin/cmart.exe ./cmd/cmart/ && echo -e 'ok\t./bin/cmart.exe'
 GOOS=darwin  GOARCH=amd64 go build -o ./bin/cmart.app ./cmd/cmart/ && echo -e 'ok\t./bin/cmart.app'
 echo '=== DONE'
